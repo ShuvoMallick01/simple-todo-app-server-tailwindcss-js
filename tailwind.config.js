ad4fc1c74e741836ef2a147484376d893ddbd9a3
ShuvoMallick01 <shuvacse01@gmail.com>
2023-07-26 18:07:32 +0600

    First Commit with Tailwind CSS using Design

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F7975B",
+        secondary: "#FECC86",
+      },
+    },
+  },
+  plugins: [],
+};
